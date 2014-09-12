ef949429f1d62dcb09c9b772d5a9021ec21fe42a
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-09-12 09:12:50 +0200

    First commit

---
+++ readfast/www/js/index.js
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        window.plugins.insomnia.keepAwake()
+        this.bindEvents();
+
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+//        var parentElement = document.getElementById(id);
+//        var listeningElement = parentElement.querySelector('.listening');
+//        var receivedElement = parentElement.querySelector('.received');
+//
+//        listeningElement.setAttribute('style', 'display:none;');
+//        receivedElement.setAttribute('style', 'display:block;');
+        console.log('lol');
+        window.plugin.notification.local.add({
+            id: '1',  // A unique id of the notifiction
+            date: new Date(2014,9,1,15,3,47,46,33),    // This expects a date object
+            message: 'Great app!',  // The message that is displayed
+            title:      'LOL',  // The title of the message
+            badge: 7,  // Displays number badge to notification
+            autoCancel: true // Setting this flag and the notification is automatically canceled when the user clicks it
+        });
+        console.log('Received Event: ' + id);
+    }
+};
